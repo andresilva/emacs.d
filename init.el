ecb15c2c1d6828c8ca6149100216d3ebfc11f9dc
Andre Silva <andre.beat@gmail.com>
2015-12-13 21:43:03 +0000

    store backup and autosave files in tmp dir

--- init.el
+++ init.el
@@ -263,6 +263,12 @@
 ;;; editor
 ;;;; settings
 
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
 ;; delete selection with a keypress
 (delete-selection-mode t)
 
