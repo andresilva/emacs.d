9bc9e9c7ebac16bfd3f00f5c5e103860d2ebb4c8
Andre Silva <andre.beat@gmail.com>
2016-01-14 11:00:45 +0000

    save history of M-x commands

--- init.el
+++ init.el
@@ -559,7 +559,7 @@
   :init
   (setq savehist-additional-variables
         ;; search entries
-        '(search-ring regexp-search-ring)
+        '(search-ring regexp-search-ring extended-command-history)
         ;; save every minute
         savehist-autosave-interval 60
         ;; keep the home clean
