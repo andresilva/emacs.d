d0d3962223fd0c340222b3f703bf495c194e90bc
Andre Silva <andre.beat@gmail.com>
2016-09-11 22:33:17 +0100

    add evil-nerd-commenter

--- init.el
+++ init.el
@@ -209,6 +209,20 @@
   (evil-mode)
   :bind ("C-q" . evil-execute-macro))
 
+;; comment and uncomment lines efficiently
+(use-package evil-nerd-commenter
+  :ensure t
+  :init
+  (evil-leader/set-key
+    "ç"  'evilnc-comment-or-uncomment-lines
+    "ci" 'evilnc-comment-or-uncomment-lines
+    "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+    "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+    "cc" 'evilnc-copy-and-comment-lines
+    "cp" 'evilnc-comment-or-uncomment-paragraphs
+    "cr" 'comment-or-uncomment-region
+    "cv" 'evilnc-toggle-invert-comment-line-by-line))
+
 ;;;; completion
 
 ;; project interaction library
