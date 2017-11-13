043aa5dd565867a3a9565d509b6cd1e00aad24f8
Andre Silva <andre.beat@gmail.com>
2017-11-13 23:56:59 +0000

    update git-gutter after magit operations

--- init.el
+++ init.el
@@ -138,6 +138,8 @@
   (set-face-foreground 'git-gutter:modified "#ffb86c") ; dracula rainbow-5
   (set-face-foreground 'git-gutter:added "#50fa7b") ; dracula rainbow-6
   (set-face-foreground 'git-gutter:deleted "#ff5555") ; dracula rainbow-9
+  (add-to-list 'git-gutter:update-hooks 'magit-post-refresh-hook)
+  (add-to-list 'git-gutter:update-hooks 'focus-in-hook)
   (global-git-gutter-mode t))
 
 ;; `evil' keys for `magit'
