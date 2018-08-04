3d9f5c9b508038acb32a14deb3bdc2c12f636b43
André Silva <andre.beat@gmail.com>
2018-08-04 15:05:38 +0100

    add git-timemachine

--- init.el
+++ init.el
@@ -264,6 +264,14 @@
   :ensure t
   :after evil magit)
 
+(use-package git-timemachine
+  :ensure t
+  :after magit
+  :config
+  (evil-make-overriding-map git-timemachine-mode-map 'normal)
+  ;; force update evil keymaps after git-timemachine-mode loaded
+  (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps))
+
 ;; display available keybindings in popup
 (use-package which-key
   :ensure t
